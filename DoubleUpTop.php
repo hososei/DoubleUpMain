1dc84cc59ae1653d7ebbd3d9b8d9bef9bbbdd708
yuhi <65522779+hososei@users.noreply.github.com>
2021-10-22 15:23:19 +0900

    Create DoubleUpTop.php

---
+++ DoubleUpTop.php
@@ -0,0 +1,14 @@
+﻿<html>
+  <head lang="ja">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+    <title>ダブルアップゲーム</title>
+  </head>
+  <form>
+    <body bgcolor="#FFDDDD">
+      <p>ダブルアップゲーム<br>
+        ダブルアップゲームの説明<br>
+      </p>
+      <input type="submit" value="ゲームを開始しますか？">
+    </body>
+  </form>
+</html>
