5f011b0d26bcefa0eb7d7496f968f79330cda54e
yuhi <65522779+hososei@users.noreply.github.com>
2021-10-22 17:32:04 +0900

    Add files via upload

---
+++ DoubleUpMain/DoubleUpChoice.php
@@ -0,0 +1,23 @@
+<html>
+  <head lang="ja">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+    <title>ダブルアップゲーム</title>
+  </head>
+  <body bgcolor="#FFDDDD">
+    <!-- ランダムに二枚のカードを抽出し片方は表、片方は裏として画像を表示する -->
+    <?php
+    //ランダムにカードを選択する処理
+    $min = 1;
+    $max = 52;
+    $openCard = 0;
+    $closeCard = 0;
+    openCard = mt-rand($min,$max);
+    closeCard = mt-rand($min,$max);
+    ?>
+    <!-- 上の関数でランダムに選んだ画像の表示 -->
+    <form>
+      <input type="submit" value="up">
+      <input type="submit" value="down">
+    </form>
+  </body>
+</html>
---
+++ DoubleUpMain/DoubleUpResult.php
@@ -0,0 +1,11 @@
+<html>
+  <head lang="ja">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+    <title>ダブルアップゲーム</title>
+  </head>
+  <body bgcolor="#FFDDDD">
+    <p>ダブルアップゲーム<br>
+    </p>
+    <input type="submit" value="ゲームを開始しますか？">
+  </body>
+</html>
